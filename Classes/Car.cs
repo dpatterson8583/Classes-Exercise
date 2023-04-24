99354bf26fd6bdfaa2e02edb4b58ecceb04b62d6
dpatterson8583 <dpatterson8583@gmail.com>
2023-04-23 22:18:10 -0500

    Initial commit for Classes Exercise

---
+++ Classes/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        //   Create a Make property of type string that is public
+        //  Create a Model property of type string that is public
+        //     Create a Year property of type int that is public
+
+        public string Make;
+        public string Model;
+        public int Year;
+
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -3,8 +3,17 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { 
+            Car newcar = new Car();
+
+            newcar.Make = "Toyota";
+            newcar.Model = "Camry";
+            newcar.Year = 2023;
+
+            Console.WriteLine($"Car Make: {newcar.Make}");
+            Console.WriteLine($"Car Model: {newcar.Model}");
+            Console.WriteLine($"Car Year: {newcar.Year}");
+
         }
     }
 }
